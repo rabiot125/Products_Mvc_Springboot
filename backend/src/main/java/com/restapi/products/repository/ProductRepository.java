1f1bbd4ac069f2b22ceb7c6edf9fbce0fc241795
Kipkoech <kipkoech.donald@turnkeyafrica.com>
2024-09-25 17:03:00 +0300

    Exposing fronted ports

--- backend/src/main/java/com/restapi/products/repository/ProductRepository.java
+++ backend/src/main/java/com/restapi/products/repository/ProductRepository.java
@@ -3,11 +3,15 @@ package com.restapi.products.repository;
 import com.restapi.products.dto.ProductDto;
 import com.restapi.products.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<ProductDto> findProductById(Long id);
+    @Query("SELECT new com.restapi.products.dto.ProductDto(p.id, p.name, p.description, p.price) " +
+            "FROM Product p WHERE p.id = :id")
+    List<ProductDto> findProductById(@Param("id") Long id);
 
 }
